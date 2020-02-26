1fc0be94603857db674760deaad30e3bf380914f
Kirk Wight <kwight@kwight.ca>
2020-02-26 11:00:53 -0800

    Add back dependency handling that was causing fatals.

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -553,7 +553,8 @@ export function generateSteps( {
 		'domains-with-preview': {
 			stepName: 'domains-with-preview',
 			apiRequestFunction: createSitelessCart,
-			providesDependencies: [ 'siteId', 'siteSlug', 'domainItem' ],
+			providesDependencies: [ 'siteId', 'siteSlug', 'domainItem', 'shouldHideFreePlan' ],
+			optionalDependencies: [ 'shouldHideFreePlan' ],
 			props: {
 				showSiteMockups: true,
 				isDomainOnly: false,
