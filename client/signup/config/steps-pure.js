5832fbef74fed4f7360d4830233e7ea29f33de03
mmtr <miguel.torres@automattic.com>
2020-03-05 16:42:51 +0100

    Hide previews on domains step

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -615,7 +615,6 @@ export function generateSteps( {
 			providesDependencies: [ 'siteId', 'siteSlug', 'domainItem', 'shouldHideFreePlan' ],
 			optionalDependencies: [ 'shouldHideFreePlan' ],
 			props: {
-				showSiteMockups: true,
 				isDomainOnly: false,
 				isSiteless: true,
 			},
